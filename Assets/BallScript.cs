c46b95804b63361917394023d808073f34ad1426
Rihuldunzubur <BH88@mail.ru>
2015-04-11 16:16:57 +0700

    Second commit
    Добавлены кирпичи и шар.

---
+++ Assets/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Скрипт мяча. Нужен для увеличения скорости и установки на начальное положение.
+/// </summary>
+public class BallScript : MonoBehaviour {
+	public float speedKoef=1f;
+	public Vector2 speed = new Vector2(3,10);
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = speed;
+		Debug.Log(rigidbody2D.velocity);
+
+	}
+
+	/// <summary>
+	/// Если столкнулись с кирпичом.
+	/// </summary>
+	/// <param name="coll">Coll.</param>
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		speed = -speed;
+		rigidbody2D.velocity = speed;
+		coll.gameObject.SendMessage("CollidedWithBall");
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BreakScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakScript : MonoBehaviour {
+	/// <summary>
+	/// Количество ударов, которое может выдержать кирпич. Если ноль - то кирпич должен быть уничтожен. Если меньше нуля - значит,
+	/// кирпич неразрушим.
+	/// </summary>
+	public int breakLives;
+	/// <summary>
+	/// Инициатор. Здесь же кирпичи перекрашиваются в нужный цвет.
+	/// </summary>
+	/// <param name="inType">Число жизней кирпича, если меньше нуля - то неуязвимый.</param>
+	public void Init( int inType)
+	{
+		breakLives = inType;
+
+	}
+
+	public void CollidedWithBall()
+	{
+		Debug.Log("bang!");
+		if ( breakLives >0) // Если равно нулю, то он уже уничтожен. Если меньше - то неуязвим.
+		{
+
+			breakLives--;
+			if (breakLives ==0)// то уничтожаем и начисляем очки.
+			{
+				DestroyBreak();
+			}
+		}
+	}
+	
+	void DestroyBreak()
+	{
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
